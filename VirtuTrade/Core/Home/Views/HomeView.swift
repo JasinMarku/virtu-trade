3cf8e3d299115fdfbcbe67af2e88b55049624bde
Jasin Marku <jasinmarkuu@gmail.com>
2024-10-18 13:24:40 -0400

    url and network error handling
    - Created a NetworkingManager class for downloading data from URLs.
    - Uses Combine framework to perform asynchronous network calls.
    - Implements error handling for bad URL responses and unknown errors.
    - Provides a download(url:) function that returns a publisher for fetched data.
    - Ensures that only successful HTTP responses (status codes 200-299) are processed.

--- VirtuTrade/Core/Home/Views/HomeView.swift
+++ VirtuTrade/Core/Home/Views/HomeView.swift
@@ -77,8 +77,8 @@ extension HomeView {
     private var allCoinsList: some View {
         List {
             ForEach(vm.allCoins) { coin in
-                    CoinRowView(coin: coin, showHoldingsColumn: false)
-                    .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+                CoinRowView(coin: coin, showHoldingsColumn: false)
+                .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
             }
         }
         .listStyle(.plain)
